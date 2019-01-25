e250da1cdaa1538e4973e30f9eba0bda6b6b90c9
Gerbi <gerbi@protonmail.com>
2019-01-24 23:36:15 -0600

    create queries graphql books, categories

---
+++ src/views/Book.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="book">
+        <ApolloQuery :query="require('@/graphql/queries/Book.gql')" :variables="{id: $route.params.id}">
+            <template slot-scope="{ result: { data, loading }, isLoading }">
+                <div v-if="isLoading">Loading...</div>
+                <div v-else>
+                    <div>{{data.book.title}}</div>
+                    <div>{{data.book.author}}</div>
+                    <!--<img :src="`http://localhost:8000/img/${book.image}`" alt="cover image">-->
+                    <img :src="`http://localhost:8000/img/${data.book.image}`" alt="book cover">
+                </div>
+            </template>
+        </ApolloQuery>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Book"
+    }
+</script>
+
+<style scoped>
+
+</style>
